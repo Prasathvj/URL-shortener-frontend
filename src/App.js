d4272b6d4678c0f055ca9d96ece6b61c80d8d10b
Prasathvj <prasathvj18@gmail.com>
2023-07-17 22:02:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import Signup from './components/Signup';
+import ForgotPassword from './components/ForgotPassword';
+import ResetPassword from './components/ResetPassword';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <div className="App">  
+    <ToastContainer theme='colored' />
+    <Routes>
+      <Route exact path='/' element={ <Login/>} />
+
+      <Route path='/dashboard' element={<Dashboard/> } />
+
+      <Route path='/signup' element={<Signup/> } />
+
+      <Route path='/forgot/password' element={<ForgotPassword/> } />
+
+      <Route path='/reset/password/:token' element={<ResetPassword/> } />
+
+    </Routes> 
+    </div>
+  );
+}
+
+export default App;
